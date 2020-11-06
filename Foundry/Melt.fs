baf8b81ccd2557c49ce7796593d9dbb6bc688939
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-11-06 23:00:21 +0100

    feat, wip: defined new signature of the melt function added partial definitions of meltBatches and meltRecords

--- Foundry/Melt.fs
+++ Foundry/Melt.fs
@@ -2,6 +2,8 @@ namespace Foundry
 
 open System.Text.RegularExpressions
 open Mold
+open Config
+open Regex
 
 /// <summary>
 /// This module contains the *melting* logic, i.e. how
@@ -32,63 +34,24 @@ module Melt =
         { Id : string
           Records : List<Record> }
 
-    // let (|ParseRegex|_|) regex str =
-    //     let mCol = Regex(regex).Matches(str)
-    //     match mCol.Count with
-    //     | 0 -> None
-    //     | _ -> Some [for m in mCol do m.Value]
 
-    // let regexSplit regex str =
-    //     let s = Regex.Split(str, regex)
-    //     if s.Length = 0
-    //     then None
-    //     else s |> Array.toList |> Some
-    
-    // module TextMelt =
+    let rec meltBatches pattern resource =
+        ()
 
-    //     let matchPartition mold (str: string) =
-    //         match str with
-    //         | ParseRegex (makeRegex mold mold.HeaderSpec) m -> m
-    //         | ParseRegex (makeRegex mold mold.QuestionUnitSpec) m -> m
-    //         | ParseRegex (makeRegex mold mold.AnswerUnitSpec) m -> m
-    //         | _ -> []
+    let rec meltRecords patternQuestion patternAnswer resource =
+        ()
 
-    //     let parseHeadings listOfBatchesAndHeadings =
-    //         [ for idx, element in (listOfBatchesAndHeadings |> List.indexed) do 
-    //           if idx % 2 = 1 then yield element ]
 
-    //     let parseBatches listOfBatchesAndHeadings =
-    //         [ for idx, element in (listOfBatchesAndHeadings |> List.indexed) do 
-    //           if idx % 2 = 0 then yield element ]
-
-    //         // [(0, "dasda")
-    //         // (2, "dasdasd")]
-
-    //     /// <summary>
-    //     /// This function is used to transform a text
-    //     /// resource into an intermediate `Record` representation.
-    //     /// The pipeline it follows:
-    //     /// 1. Find all matching structures delimited by the *Magic Marker*.
-    //     /// 2. Look back from each odd match of the *Magic Marker* to try and
-    //     ///    find the header that precedes a section. If a header is not found,
-    //     ///    write an empty string instead of a header into the `Record` instance.
-    //     /// 3. Compartmentalize the aforementioned structures into a `List<string>`,
-    //     ///    with the headers stored in a separate transient `List<string>`.
-    //     /// 4. For each string in the data list, apply `QuestionUnitRegex` to obtain
-    //     ///    the spec of the first data field that by convention represents a
-    //     ///    question unit. Compartmentalize underlying answer units on the question
-    //     ///    unit, i.e. feed through all the potential answer units with the
-    //     ///    question unit stripped out.
-    //     /// 5. 
-    //     let melt (mold: TextMold) resource =
-    //         match regexSplit mold.MagicMarker resource with
-    //         | Some listOfBatchesAndHeadings -> parseBatches
-    //         | None -> List.empty
-    //         // ["pre # title", "records", 
-    //         // "pre # title", "records"]
-
-    // let melt mold resource =
-    //     match mold with
-    //     | MarkdownMold mold -> TextMelt.melt mold resource
-
-    
+    let melt config mold resource =
+        // Convert tuple to seq, map interpolation to each element:
+        let pBatch, pQuestion, pAnswer = 
+            carveMoldMelt mold 
+            |> fun (i, j, k) -> seq { i; j; k; }
+            |> Seq.map (interpolateMarkers <| 
+                        regexMoldInterpolationMap config.MagicMarker)
+            |> fun i -> Seq.item 0 i, Seq.item 1 i, Seq.item 2 i  // back to tuple
+        // Melt walking through the parse tree:
+        // TODO: recursively parse records until there are no more,
+        //       same for batches, i.e. `meltBatches` and `meltRecords`
+        //       should be recursive
+        meltBatches pBatch resource |> meltRecords pQuestion pAnswer
